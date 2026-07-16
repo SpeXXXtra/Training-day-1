f71fc31de0208dae6ee6a4add33f5f548c16d70e
Nihal Sulthan <nihal.sultan@gmail.com>
2026-07-16 14:08:40 +0530

    commit 2

---
+++ exe.js
@@ -0,0 +1,8 @@
+function getDetails(){
+    name = document.getElementById("name").value;
+    age = document.getElementById("age").value;
+    lastname = document.getElementById("lastname").value;
+    color = document.getElementById("color").value;
+    degree = document.getElementById("degree").value;
+    alert("Name: " + name + "\nAge: " + age + "\nLast Name: " + lastname + "\nColor: " + color + "\nDegree: " + degree);
+}
